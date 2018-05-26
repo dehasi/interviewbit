09aa2cdd77e063ac45ef9151855096387affcbd6
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-05-26 21:01:36 +0200

    IB-2A Implement spiral for square matrix

--- src/main/java/arrays/primers/SpriralOrderMatrix.java
+++ src/main/java/arrays/primers/SpriralOrderMatrix.java
@@ -5,7 +5,59 @@ import java.util.List;
 
 public class SpriralOrderMatrix {
     // DO NOT MODIFY THE LIST. IT IS READ ONLY
-    public List<Integer> spiralOrder(final List<ArrayList<Integer>> A) {
-        throw new RuntimeException();
+    public ArrayList<Integer> spiralOrder(final List<ArrayList<Integer>> A) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = Math.max(A.size() / 2, A.get(0).size()/2);
+        for (int l = 0; l < n; l++) {
+            result.addAll(cut(A, l));
+        }
+//        if(A.size() % 2 != 0) {
+//            int x = n;
+//            result.add(A.get(x).get(x));
+//        }
+        return result;
+    }
+
+
+    List<Integer> cut(final List<ArrayList<Integer>> matrix, int layer) {
+        List<Integer> list = new ArrayList<>();
+        int xWith = matrix.size()-1;
+        int yHeight = matrix.size()-1;
+
+        P p1 = new P(0+layer, 0 + layer);
+        P p2 = new P(xWith-layer, 0 + layer);
+        P p3 = new P(xWith-layer, yHeight - layer);
+        P p4 = new P(0+layer, yHeight - layer);
+
+        for (int x = p1.x; x < p2.x; x++) {
+            int point = matrix.get(p1.y).get(x);
+            list.add(point);
+        }
+        for (int y = p2.y; y < p3.y; y++) {
+            int point = matrix.get(y).get(p2.x);
+            list.add(point);
+        }
+        for (int x = p3.x; x > p4.x; x--) {
+            int point = matrix.get(p3.y).get(x);
+            list.add(point);
+        }
+        for (int y = p4.y; y > p1.y; y--) {
+            int point = matrix.get(y).get(p4.x);
+            list.add(point);
+        }
+        return list;
+    }
+    static class P {
+        final int x;
+        final int y;
+
+        P(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public String toString() {
+            return String.format("{x:%s, y:%s}", x, y);
+        }
     }
 }
--- src/test/java/arrays/primers/SpriralOrderMatrixTest.java
+++ src/test/java/arrays/primers/SpriralOrderMatrixTest.java
@@ -11,8 +11,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SpriralOrderMatrixTest {
     private final SpriralOrderMatrix matrix = new SpriralOrderMatrix();
 
+
     @Test
-    public void test() {
+    public void spiralOrder_1x1_ok() {
+        List<ArrayList<Integer>> test = new ArrayList<>(asList(
+                new ArrayList<>(asList(1, 1))
+        ));
+
+        List<Integer> result = matrix.spiralOrder(test);
+        assertThat(result).containsExactly(1);
+    }
+
+    @Test
+    public void spiralOrder_2x2_ok() {
+        List<ArrayList<Integer>> test = new ArrayList<>(asList(
+                new ArrayList<>(asList(1, 2)),
+                new ArrayList<>(asList(3, 4))
+        ));
+
+        List<Integer> result = matrix.spiralOrder(test);
+        assertThat(result).containsExactly(1, 2, 4, 3);
+    }
+
+    @Test
+    public void spiralOrder_3x3_ok() {
         List<ArrayList<Integer>> test = new ArrayList<>(asList(
                 new ArrayList<>(asList(1, 2, 3)),
                 new ArrayList<>(asList(4, 5, 6)),
@@ -21,5 +43,31 @@ public class SpriralOrderMatrixTest {
 
         List<Integer> result = matrix.spiralOrder(test);
         assertThat(result).containsExactly(1, 2, 3, 6, 9, 8, 7, 4, 5);
+    }
+
+    @Test
+    public void spiralOrder_3x2_ok() {
+        List<ArrayList<Integer>> test = new ArrayList<>(asList(
+                new ArrayList<>(asList(1, 2)),
+                new ArrayList<>(asList(3, 4)),
+                new ArrayList<>(asList(5, 6))
+        ));
+
+        List<Integer> result = matrix.spiralOrder(test);
+        assertThat(result).containsExactly(1, 2, 4, 6, 5, 3);
+    }
+
+    @Test
+    public void spiralOrder_4x4_ok() {
+        List<ArrayList<Integer>> test = new ArrayList<>(asList(
+                new ArrayList<>(asList(1, 2, 3, 4)),
+                new ArrayList<>(asList(5, 6, 7, 8)),
+                new ArrayList<>(asList(9, 10, 11, 12)),
+                new ArrayList<>(asList(13, 14, 15, 16))
+
+        ));
+
+        List<Integer> result = matrix.spiralOrder(test);
+        assertThat(result).containsExactly(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10);
     }
 }
