f949c0256387093e6d4af7843ae8be8bd1fd017a
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-05-28 01:02:28 +0200

    IB-2A Implement Find Duplicate In Array

--- src/main/java/arrays/bucketingorsorting/FindDuplicateInArray.java
+++ src/main/java/arrays/bucketingorsorting/FindDuplicateInArray.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class FindDuplicateInArray {
     public int repeatedNumber(final List<Integer> a) {
+        int n = a.size() - 1;
+        boolean[] hash = new boolean[n + 1];
+        for (int i = 0; i < a.size(); i++) {
+            int val = a.get(i);
+            if (hash[val]) return val;
+            hash[val] = true;
+        }
         return -1;
     }
 }
--- src/test/java/arrays/bucketingorsorting/FindDuplicateInArrayTest.java
+++ src/test/java/arrays/bucketingorsorting/FindDuplicateInArrayTest.java
@@ -2,6 +2,8 @@ package arrays.bucketingorsorting;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +13,13 @@ public class FindDuplicateInArrayTest {
     private final FindDuplicateInArray findDuplicateInArray = new FindDuplicateInArray();
 
     @Test
-    public void repeatetNumber_n4_ok() {
-        assertThat(findDuplicateInArray.repeatedNumber(asList(3, 4, 1, 4, 1))).isEqualTo(1);
+    public void repeatedNumber_n4_ok() {
+        List<Integer> input = asList(3, 4, 1, 4, 1);
+        List<Integer> duplicates = asList(4, 1);
+
+        int result = findDuplicateInArray.repeatedNumber(input);
+
+        assertThat(duplicates).contains(result);
     }
 
 }
